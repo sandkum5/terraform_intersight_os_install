4d995263a7daebae5b2b95ad4537c56872624230
Sandeep Kumar <58764439+sandkum5@users.noreply.github.com>
2022-12-21 14:02:12 -0800

    Update data.tf

--- os_install/data.tf
+++ os_install/data.tf
@@ -9,3 +9,11 @@ data "intersight_compute_physical_summary" "server" {
 data "intersight_os_configuration_file" "os_config" {
     name = var.os_config_file
 }
+
+data "intersight_softwarerepository_operating_system_file" "os_repo" {
+    name = var.os_repo_name
+}
+
+data "intersight_firmware_server_configuration_utility_distributable" "scu_repo" {
+    name = var.scu_repo_name
+}
